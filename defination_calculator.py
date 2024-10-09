902ab11c34e32e0649c9ee6b3c327369b6e06ac5
Nepobedimyi-Wef <151131838+Nepobedimyi-Wef@users.noreply.github.com>
2024-10-09 09:51:04 +0700

    Update defination_calculator.py

--- defination_calculator.py
+++ defination_calculator.py
@@ -18,8 +18,8 @@ def division(num_1, num_2):
 num_1 = int(input("Enter a number 1: "))
 num_2 = int(input("Enter a number 2: "))
 
-force_number(num_1,num_2)
-summ(num_1,num_2)
-subtraction(num_1,num_2)
-multiplication(num_1,num_2)
-division(num_1,num_2)
+force_number(num_1, num_2)
+summ(num_1, num_2)
+subtraction(num_1, num_2)
+multiplication(num_1, num_2)
+division(num_1, num_2)
