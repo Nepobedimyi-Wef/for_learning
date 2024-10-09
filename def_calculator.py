fa752930ab90ad96e68f73d8f9388a5507bb379e
Nepobedimyi-Wef <151131838+Nepobedimyi-Wef@users.noreply.github.com>
2024-10-09 09:47:01 +0700

    Create def_calculator.py

---
+++ def_calculator.py
@@ -0,0 +1,21 @@
+def summ(num_1, num_2):
+    print(f"Summ of number: {num_1 + num_2}")
+
+def subtraction(num_1, num_2):
+    print(f"Subtraction of number: {num_1 - num_2}")
+
+def multiplication(num_1, num_2):
+    print(f"Multiplication of number: {num_1 * num_2}")
+def division(num_1, num_2):
+    try:
+        print(f"Division of number: {num_1 / num_2}")
+    except ZeroDivisionError:
+        print("You can't divide by zero")
+
+num_1 = int(input("Enter a number 1: "))
+num_2 = int(input("Enter a number 2: "))
+
+summ(num_1,num_2)
+subtraction(num_1,num_2)
+multiplication(num_1,num_2)
+division(num_1,num_2)
